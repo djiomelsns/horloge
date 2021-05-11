813bd7d37dbaf8dcc67f33b67ea71966bc4e4a14
mohamedbelaidene <djiomel2001@gmail.com>
2021-05-11 22:57:31 +0100

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+function setTime(){
+  var date =new Date();
+  var time = document.querySelector(".time-T");
+  time.innerHTML = date.getHours() + ":" +date.getMinutes();
+  var data = document.querySelector(".time-D");
+  data.innerHTML =date.getDay() +"-"+parseInt(date.getMonth()+1)+"-"+date.getFullYear() ;
+}
+setInterval(setTime,100);
+onload = setTime();
+
+function changeColor(){
+    var colors = document.querySelector(".colors").value;
+    localStorage.setItem("couleur",colors);
+    document.querySelector(".time").style.background = localStorage.getItem("couleur");
+    document.body.style.background = localStorage.getItem("couleur");
+}
+function setcolor(){
+  document.querySelector(".colors").value =localStorage.getItem("couleur")
+  document.querySelector(".time").style.background = localStorage.getItem("couleur");
+  document.body.style.background = localStorage.getItem("couleur");
+}
+onload = setcolor;
+
